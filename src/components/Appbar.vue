8612a3083c15656c4ceaf5ab0ac263b06eb1ab72
TruogAnh-28 <nguyentruongtuananh03@gmail.com>
2024-11-07 19:57:37 +0700

    feat: add sidebar, appbar, router

---
+++ src/components/Appbar.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app-bar :elevation="0">
+    <!-- <template v-slot:prepend>
+       <v-text-field
+      :loading="loading"
+      append-inner-icon="mdi-magnify"
+      density="compact"
+      label="Search templates"
+      variant="solo"
+      hide-details
+      single-line
+      class="search-bar"
+      @click:append-inner="onClick"
+      max-width="50%"
+    ></v-text-field>
+
+    </template> -->
+<!-- 
+    <v-text-field
+      :loading="loading"
+      append-inner-icon="mdi-magnify"
+      density="compact"
+      label="Search"
+      variant="solo"
+      hide-details
+      single-line
+      class="search-bar"
+      @click:append-inner="onClick"
+      max-width="50%"
+    ></v-text-field> -->
+
+    <template v-slot:append>
+      <v-spacer></v-spacer>
+      <v-btn icon="mdi-bell-outline"></v-btn>
+      <v-btn icon="mdi-email-box"></v-btn>
+      <v-list-item
+                prepend-avatar="@/assets/logo.png"
+                subtitle="nat_nhom6dacn"
+                title="NAT"
+      ></v-list-item>
+    </template>
+  </v-app-bar>
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
+<style scoped>
+.search-bar {
+  margin-left: 2rem;
+}
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,24 @@
+<template>
+      <v-navigation-drawer
+        expand-on-hover
+        rail
+      >
+        <v-list>
+          <v-list-item
+            prepend-avatar="@/assets/codemate.png"
+            title="CODEMATE"
+         
+          ></v-list-item>
+        </v-list>
+        <v-divider></v-divider>
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-view-dashboard" title="Dashboard" value="dashboard" :to="'/dashboard'" router></v-list-item>
+          <v-list-item prepend-icon="mdi-school" title="Course" value="course" :to="'/courselist'" router></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main style="height: 250px"></v-main>
+</template>
+<script setup lang="ts">
+
+</script>
