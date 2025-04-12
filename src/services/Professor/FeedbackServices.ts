911b29d99871cea72cdadee860c2bc9aa11efbee
Thi <lsjenny1710@gmail.com>
2025-04-12 14:30:35 +0700

    feat: enhance feedback functionality and update routing

--- src/services/Professor/FeedbackServices.ts
+++ src/services/Professor/FeedbackServices.ts
@@ -1,15 +1,15 @@
 import ApiService from "@/common/api.service";
+import { IResponseData } from "@/modals/apis/response";
 import { AuthConfig } from "@/services/authenServices";
-import {
-    GetListFeedbackProfessorResponse,
-} from "@/types/Feedback";
+import { GetListFeedbackProfessorResponse } from "@/types/Feedback";
 
 export const feedbackServices = {
-    async fetchFeedbackProfessor({ showError, showSuccess }: AuthConfig,
-        course_id: string) {
-        return await ApiService.query<GetListFeedbackProfessorResponse[]>(
-            `feedback/courses/${course_id}`,
-            { showError, showSuccess }
-        );
-    },
+  async fetchFeedbackProfessor(
+    { showError, showSuccess }: AuthConfig,
+    course_id: string
+  ) {
+    return await ApiService.get<
+      IResponseData<GetListFeedbackProfessorResponse[]>
+    >(`/feedback/courses/${course_id}`, "", { showError, showSuccess });
+  },
 };
